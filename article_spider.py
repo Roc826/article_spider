2571c13dac4d0d279b9472fac61ed98116e04fcd
Galaxyzeta <54425661+Galaxyzeta@users.noreply.github.com>
2019-11-25 16:41:23 +0800

    Galaxyzeta's Update
    删除不常用库，引入常用爬虫库，重写代码，支持爬取多内容如糗事百科一页上的很多的笑话

--- article_spider.py
+++ article_spider.py
@@ -1,9 +1,18 @@
-import requests_html
+'''
+Modified by @Galaxyzeta
+1.把库换成爬虫常见库，如requests+bs4组合
+2.能爬多个被选择的html部分，如糗事百科一页上的所有笑话
+3.selector样例：selector={'class':'123', 'id':'haha'}
+4.测试样例: http://www.lovehhy.net/Joke/Detail/QSBK/25, {"selector": "{'id':'endtext'}", "cookie": "none"}
+'''
+import requests
 import html2text
 import os
 import json
+import bs4
 from urlparser import urlparser
 from module.Printer import printer
+
 SITE_CONFIG_PATH = './config/website/{}.json'
 BASE_CONFIG_PATH = './config/base.json'
 
@@ -12,6 +21,13 @@ netloc = urlparser.urlparse(url)[1]
 site = netloc.split(".")[-2]
 siteConfigExist = 'n'
 baseConfigExist = 'n'
+siteConfig={
+    "selector": None,
+    "cookie": None
+}
+baseConfig={
+    "path": None
+}
 
 if os.path.isfile(SITE_CONFIG_PATH.format(site)):
     siteConfigExist = input(printer.info("{}配置已存在,是否使用该配置(y/n):".format(site))).lower()
@@ -32,13 +48,18 @@ if siteConfigExist == 'n':
     with open(SITE_CONFIG_PATH.format(site),"w") as file:
         json.dump(siteConfig, file)
 
-session = requests_html.HTMLSession()
+session = requests.Session()
 headers = {
     "cookie": siteConfig["cookie"]
 }
-request = session.get(url, headers=headers)
-a = request.html.find(siteConfig["selector"],first=True).html
-markdown = html2text.html2text(a)+"\n文章来源："+request.url
+html = session.get(url, headers=headers).content.decode("gbk")
+soup = bs4.BeautifulSoup(html, "html.parser")
+li = soup.findAll(attrs=eval(str(siteConfig["selector"])))
+stringbuilder = ""
+for i in range(len(li)):
+    stringbuilder += str(li[i])
+
+markdown = html2text.html2text(stringbuilder)
 
 if os.path.isfile(BASE_CONFIG_PATH):
     baseConfigExist = input(printer.info("路径配置已存在,是否使用该配置(y/n):".format(site))).lower()
